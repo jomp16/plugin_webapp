f554cdc67a891c3d074b5445b1563df549fdfc0f
José Olívio M. Pedrosa <root@rwx.ovh>
2018-06-28 22:53:24 -0300

    Implement pocket Habbo API routes
    Signed-off-by: José Olívio M. Pedrosa <root@rwx.ovh>

---
+++ src/main/kotlin/ovh/rwx/utils/plugin/webapp/controllers/api/pocket/PocketHabboController.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2015-2018 jomp16 <root@rwx.ovh>
+ *
+ * This file is part of plugin_webapp.
+ *
+ * plugin_webapp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * plugin_webapp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with plugin_webapp. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ovh.rwx.utils.plugin.webapp.controllers.api.pocket
+
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.*
+import ovh.rwx.habbo.database.user.UserInformationDao
+import ovh.rwx.habbo.game.user.information.UserInformation
+import ovh.rwx.utils.plugin.webapp.controllers.api.pocket.json.LoginPocketHabbo
+import java.util.*
+import javax.servlet.http.HttpSession
+
+@RestController
+class PocketHabboController {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @GetMapping("/api/public/info/hello")
+    fun helloWorld(): Map<String, String> {
+        log.debug("Got hello!")
+
+        return mapOf("message" to "hello world")
+    }
+
+    @PostMapping("/api/public/authentication/login")
+    fun login(@RequestBody loginPocketHabbo: LoginPocketHabbo, httpSession: HttpSession): Map<String, String> {
+        log.debug("Got login!")
+
+        val userInformation = UserInformationDao.getUserInformationByEmailAndPassword(loginPocketHabbo.email, loginPocketHabbo.password)
+                ?: return mapOf("message" to "falha-login")
+
+        httpSession.setAttribute("userInformation", userInformation)
+
+        return mapOf("message" to "ok")
+    }
+
+    @GetMapping("/api/ssotoken")
+    fun ssoToken(@SessionAttribute("userInformation") userInformation: UserInformation?): Map<String, String> {
+        log.debug("Got ssotoken!")
+
+        if (userInformation == null) return mapOf("message" to "error-auth")
+
+        val ssoToken = UUID.randomUUID().toString()
+
+        UserInformationDao.updateAuthTicket(userInformation, ssoToken)
+
+        return mapOf("ssoToken" to ssoToken)
+    }
+}
---
+++ src/main/kotlin/ovh/rwx/utils/plugin/webapp/controllers/api/pocket/json/LoginPocketHabbo.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015-2018 jomp16 <root@rwx.ovh>
+ *
+ * This file is part of plugin_webapp.
+ *
+ * plugin_webapp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * plugin_webapp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with plugin_webapp. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ovh.rwx.utils.plugin.webapp.controllers.api.pocket.json
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class LoginPocketHabbo(
+        @JsonProperty("email")
+        val email: String,
+        @JsonProperty("password")
+        val password: String
+)
